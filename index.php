24f8a6b23eb93d95a2509f0cb5f1911c3b878591
Daniel W. Robert <danielwrobert@gmail.com>
2020-04-21 16:32:03 -0400

    Move script translation setting outside of block loop.

--- index.php
+++ index.php
@@ -56,17 +56,16 @@ function gutenberg_blocks_demo_register_block() {
 				'editor_script' => 'gutenberg-blocks-demo',
 			]
 		);
-
-		if ( function_exists( 'wp_set_script_translations' ) ) {
-			/**
-			 * May be extended to wp_set_script_translations( 'my-handle', 'my-domain',
-			 * plugin_dir_path( MY_PLUGIN ) . 'languages' ) ). For details see
-			 * https://make.wordpress.org/core/2018/11/09/new-javascript-i18n-support-in-wordpress/
-			 */
-			wp_set_script_translations( $block, 'gutenberg-blocks-demo' );
-		}
 	}
-
+	
+	if ( function_exists( 'wp_set_script_translations' ) ) {
+		/**
+		 * May be extended to wp_set_script_translations( 'my-handle', 'my-domain',
+		 * plugin_dir_path( MY_PLUGIN ) . 'languages' ) ). For details see
+		 * https://make.wordpress.org/core/2018/11/09/new-javascript-i18n-support-in-wordpress/
+		 */
+		wp_set_script_translations( $block, 'gutenberg-blocks-demo' );
+	}
 }
 add_action( 'init', 'gutenberg_blocks_demo_register_block' );
 
